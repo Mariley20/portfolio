5c76d1e188d79c839ecc5d618334d917d2132085
Mariley20 <mariley2018@gmail.com>
2017-11-08 17:32:52 -0500

    css

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Row, Col, ListGroup, ListGroupItem} from 'react-bootstrap';
-
+// import { Row, Col, ListGroup, ListGroupItem} from 'react-bootstrap';
+import './styles/App.css';
 // import { evaluateAddCard, addCard } from './actions';
 export const Home = () => {
 return (
     <div>
-        <Row>
-            <Col sm={3}>
-                <ListGroup className="ListGroup">
-                    <ListGroupItem>Home</ListGroupItem>
-                    <ListGroupItem>About</ListGroupItem>
-                    <ListGroupItem>Portfolio</ListGroupItem>
-                    <ListGroupItem>Contact</ListGroupItem>
-                </ListGroup>
-            </Col>
-            <Col sm={5}>
-            Homeee
-            </Col>
-        </Row>
+       <div class="row">
+    <div class="col-sm-3">
+        <ul class="ListGroup list-group">
+            <li class="list-group-item">Home</li>
+            <li class="list-group-item">About</li>
+            <li class="list-group-item">Portfolio</li>
+            <li class="list-group-item">Contact</li>
+        </ul>
+    </div>
+    <div class="col-sm-5">Homeee</div>
+</div>
     </div>
 )
 }
