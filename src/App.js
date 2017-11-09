5fabe7136cbe68813782859538709eca70bd02cc
Mariley20 <mariley2018@gmail.com>
2017-11-09 14:55:53 -0500

    create componentes

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ import {
     Switch} from 'react-router-dom';
 // import './styles/App.css';
 import {Home} from './Home';
-// import {About} from 'About';
-// import {Portfolio} from './Portfolio';
-// import {Resume} from './Resume';
+import {About} from 'About';
+import {Portfolio} from './Portfolio';
+import {Resume} from './Resume';
 const App = ({myBoard, selected}) => {
 return (
      <div className="container-fluid">
         <BrowserRouter>
             <Switch>
                     <Route exact path="/" render={() => <Home myBoard={myBoard} selected={selected} />}/>
-                    {/* <Route path="/about" render={() => <About myBoard={myBoard} selected={selected} />}/>
+                    <Route path="/about" render={() => <About myBoard={myBoard} selected={selected} />}/>
                     <Route path="/portfolio"  render={() => <Portfolio myBoard={myBoard} selected={selected} />}/>
-                    <Route path="/contact"  render={() => <Portfolio myBoard={myBoard} selected={selected} />}/>
-                    <Route path="/resume"  render={() => <Resume myBoard={myBoard} selected={selected} />}/> */}
+                    <Route path="/contact"  render={() => <Contact myBoard={myBoard} selected={selected} />}/>
+                    <Route path="/resume"  render={() => <Resume myBoard={myBoard} selected={selected} />}/>
             </Switch>
         </BrowserRouter>
     </div>
