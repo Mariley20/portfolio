03a98f9e4673449db9ab199948d2a9e820771832
Mariley20 <mariley2018@gmail.com>
2017-11-24 07:57:53 -0500

    navbar lef fixed

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -7,14 +7,16 @@ import './css/app.css';
 export const Portfolio = ({NavBar, Information, work}) => {
 return (
     <div className="row">
+        <div className='fixed'>
         <div className="col-sm-2 col-lg-2 remove-left remove-right">
-                <NavBar />
+            <NavBar />
         </div>
         <div className="col-md-3 col-sm-3 col-lg-3 remove-left">
            <Information />
         </div>
-        <div className="col-md-7">
-        <Content work={work} />
+        </div>
+        <div className="col-md-7 content-portfolio">
+            <Content work={work} />
         </div>
     </div>)
 }
